3eeffff22451021c680860a21d0bffc229654aa8
Tosking <tosking@duck.com>
2022-11-23 23:55:33 +0500

    fixed warning in auth.php

--- auth.php
+++ auth.php
@@ -26,8 +26,8 @@
         $login = htmlspecialchars($_POST['login'], ENT_QUOTES, 'UTF-8');
         $pass = md5($_POST['pass']);
 
-        $user = $pdo->query('SELECT * FROM user WHERE login = "'.$login.'"');
-        if($user){
+        $user = $pdo->query('SELECT 1 FROM user WHERE login = "'.$login.'"');
+        if(isset($user->password)){
             $user = $user->fetch(PDO::FETCH_OBJ);
             if($user->password != $pass){
                 $msg = "Неверный логин или пароль";
@@ -52,7 +52,7 @@
         </div>
         <form method="post" action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>">
             <div class="input-group input-group-sm mx-auto mb-5 w-75 row">
-                <input style="border: none; border-bottom: 1px solid; border-radius: 0px;" type="text" class="justify-content-center form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-sm" placeholder="Email" name="login">
+                <input style="border: none; border-bottom: 1px solid; border-radius: 0px;" type="text" class="justify-content-center form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-sm" placeholder="Login" name="login">
             </div>
             <div class="input-group input-group-sm mx-auto mb-5 w-75 row">
                 <input style="border: none; border-bottom: 1px solid; border-radius: 0px;" type="text" class="justify-content-center form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-sm" placeholder="Password" name="pass">
@@ -60,7 +60,7 @@
         <?php
             if(isset($_POST['login']) && !empty($_POST['pass'])){
                 if(isset($msg)){
-                    echo '<div class="text-center w-50 row mb-3 justify-content-lg-center" style="color: red;">'.$msg.'</div>';
+                    echo '<div class="text-center row mb-3 justify-content-center" style="color: red;">'.$msg.'</div>';
                 }
             }
         ?>
